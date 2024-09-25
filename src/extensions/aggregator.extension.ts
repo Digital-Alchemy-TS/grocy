3c7d0aee753edd581b8ac10a7a5d843310d0e7eb
Zoe <git@zoe-codez.xyz>
2024-09-25 08:58:02 -0500

    Upgrades (#3)
    * wip
    
    * fixes
    
    * bump

--- src/extensions/aggregator.extension.ts
+++ src/extensions/aggregator.extension.ts
@@ -1,13 +1,8 @@
-import {
-  CronExpression,
-  eachLimit,
-  TServiceParams,
-} from "@digital-alchemy/core";
+import { CronExpression, eachLimit, TServiceParams } from "@digital-alchemy/core";
 
 import {
   BATTERY_CACHE_UPDATED,
   CHORE_CACHE_UPDATED,
-  GROCY_REBUILD_CACHE,
   GrocyBattery,
   GrocyBatteryUserfields,
   GrocyChore,
@@ -17,19 +12,11 @@ import {
   TASK_CACHE_UPDATED,
 } from "../helpers";
 
-export function Aggregator({
-  scheduler,
-  logger,
-  grocy,
-  event,
-  config,
-  lifecycle,
-}: TServiceParams) {
+export function Aggregator({ scheduler, logger, grocy, event, config, lifecycle }: TServiceParams) {
   async function refresh() {
     await aggregator.buildBatteryCache();
     await aggregator.buildChoresCache();
     await aggregator.buildTaskCache();
-    GROCY_REBUILD_CACHE.setToCurrentTime();
   }
   lifecycle.onBootstrap(async () => await refresh());
   scheduler.cron({
@@ -46,21 +33,16 @@ export function Aggregator({
       const battery = await grocy.battery.listBatteries();
       const cache = new Set<GrocyBattery>();
 
-      await eachLimit(
-        battery,
-        config.grocy.USERFIELDS_FETCH_RATE,
-        async battery => {
-          const userfields =
-            await grocy.object.listObjectUserFields<GrocyBatteryUserfields>({
-              id: battery.id.toString(),
-              type: "batteries",
-            });
-          cache.add({
-            ...battery,
-            userfields,
-          });
-        },
-      );
+      await eachLimit(battery, config.grocy.USERFIELDS_FETCH_RATE, async battery => {
+        const userfields = await grocy.object.listObjectUserFields<GrocyBatteryUserfields>({
+          id: battery.id.toString(),
+          type: "batteries",
+        });
+        cache.add({
+          ...battery,
+          userfields,
+        });
+      });
       aggregator.BATTERY_CACHE = cache;
       event.emit(BATTERY_CACHE_UPDATED);
       logger.debug("Battery cache updated");
@@ -70,16 +52,10 @@ export function Aggregator({
       const chores = await grocy.chores.listChores();
       const cache = new Set<GrocyObjectChoreDetail & GrocyChore>();
 
-      await eachLimit(
-        chores,
-        config.grocy.USERFIELDS_FETCH_RATE,
-        async chore => {
-          const data = (await grocy.chores.getChoreObject(
-            chore.id,
-          )) as GrocyObjectChoreDetail;
-          cache.add({ ...chore, ...data });
-        },
-      );
+      await eachLimit(chores, config.grocy.USERFIELDS_FETCH_RATE, async chore => {
+        const data = (await grocy.chores.getChoreObject(chore.id)) as GrocyObjectChoreDetail;
+        cache.add({ ...chore, ...data });
+      });
       aggregator.CHORES_CACHE = cache;
       event.emit(CHORE_CACHE_UPDATED);
       logger.debug("Chore cache updated");
@@ -90,11 +66,10 @@ export function Aggregator({
       const cache = new Set<GrocyTask>();
 
       await eachLimit(tasks, config.grocy.USERFIELDS_FETCH_RATE, async task => {
-        const userfields =
-          await grocy.object.listObjectUserFields<GrocyTaskUserfields>({
-            id: task.id.toString(),
-            type: "tasks",
-          });
+        const userfields = await grocy.object.listObjectUserFields<GrocyTaskUserfields>({
+          id: task.id.toString(),
+          type: "tasks",
+        });
         cache.add({
           ...task,
           userfields,
